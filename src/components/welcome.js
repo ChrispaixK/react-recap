2d5d42a1754be08c8110513ee45ee1a6984bd25b
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-01-09 11:32:07 +0200

    remove old code and add starter files

---
+++ src/components/welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Welcome = (props) => (
+  <div>
+    <div>
+      <h2>
+        Welcome to react recap
+        {props}
+      </h2>
+    </div>
+
+  </div>
+);
+
+export default Welcome;
