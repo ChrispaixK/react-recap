23c6ffc1efd7fad416a5cf5070f9c61f55a2ee6a
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-02-08 00:04:07 +0200

    Add expenses styles and fix style linters errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Expenses from './components/Expenses';
+import './components/Expenses.css';
 
 function App() {
   const expenses = [
