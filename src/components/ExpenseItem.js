dcdda7733172520b96d12f0b48e357fcc1552522
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-02-07 12:47:40 +0200

    dynamic components passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => (
-  <div className="expense-item">
-    <div>March 28th 221</div>
-    <div className="expense-item_description">
-      <h2>Car Insurance</h2>
-      <div className="expense-item_price">$239</div>
+const ExpenseItem = (props) => {
+  const {
+    id, title, amount, date,
+  } = props;
+
+  return (
+    <div id={id} className="expense-item">
+      <div>{date.toDateString()}</div>
+      <div className="expense-item_description">
+        <h2>{title}</h2>
+        <div className="expense-item_price">{amount}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+ExpenseItem.propTypes = {
+  id: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  amount: propTypes.number.isRequired,
+  date: propTypes.string.isRequired,
+};
 
 export default ExpenseItem;
