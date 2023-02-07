cd8281638e11b71273136f57a2ad9914ab3c54ac
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-02-07 23:56:22 +0200

    splitting component into multiple components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 import Header from './components/Header';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -31,34 +31,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
   return (
     <div>
       <Header />
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <div className="expenses">
+        <Expenses expensesList={expenses} />
+      </div>
     </div>
   );
 }
